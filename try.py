d4844bee5bc7ea666b48e9cd2badc380aa7d39d7
Milcomeda <86146232+Milcomeda@users.noreply.github.com>
2021-08-06 07:04:10 +0900

    グラフ グリグリ

--- try.py
+++ try.py
@@ -1,4 +1,26 @@
-def greeting(name):
-    print("Hello,", name)
+import numpy as np
+xs = np.linspace(0, 10, 100)
+sins = np.sin(xs)
+randoms = np.random.rand(100)
 
-greeting("World")
+import plotly.graph_objects as go
+fig = go.Figure(data=[
+    go.Scatter(x=xs, y=sins, name="sin"),
+    go.Scatter(x=xs, y=randoms, name="random"),
+])
+fig.show()
+
+fig.update_xaxes(title="x") # X軸タイトルを指定
+fig.update_yaxes(title="y") # Y軸タイトルを指定
+
+fig.update_xaxes(range=(1,3)) # X軸の最大最小値を指定
+fig.update_xaxes(rangeslider={"visible":True}) # X軸に range slider を表示（下図参照）
+
+fig.update_yaxes(scaleanchor="x", scaleratio=1) # Y軸のスケールをX軸と同じに（plt.axis("equal")）
+
+fig.update_layout(title="Title") # グラフタイトルを設定
+fig.update_layout(font={"family":"Meiryo", "size":20}) # フォントファミリとフォントサイズを指定
+fig.update_layout(showlegend=True) # 凡例を強制的に表示（デフォルトでは複数系列あると表示）
+fig.update_layout(xaxis_type="linear", yaxis_type="log") # X軸はリニアスケール、Y軸はログスケールに
+fig.update_layout(width=800, height=600) # 図の高さを幅を指定
+fig.update_layout(template="plotly_white") # 白背景のテーマに変更
